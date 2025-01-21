57f3a943b195f42fc46bac02b0a57e8ff6f00fe4
Sarah Zwart <sarahczwart@gmail.com>
2025-01-20 20:17:41 -0500

    trying to resolve errors

--- src/app/(others)/page.tsx
+++ src/app/(others)/page.tsx
@@ -1,23 +1,33 @@
 import Input from "@/components/Input";
 import Feed from "@/components/Feed";
 
+interface PostInterface {
+  comments: string[];
+  createdAt: string;
+  image: string;
+  likes: string[];
+  name: string;
+  profileImg: string;
+  text: string;
+  updatedAt: string;
+  user: string;
+  username: string;
+  __v: number;
+  _id: string;
+}
+
 export default async function Home() {
-  let data = null;
-  try {
+  let data: PostInterface[] = [];
+  try{
     const result = await fetch(process.env.URL + '/api/post/all', {
       method: 'GET',
-      cache: 'no-store',
+      cache: 'no-store'
     });
-    data = await result.json();
-  } catch (error) {
-    console.error('Error fetching posts:', error);
+    data = await result.json()
+    console.log(data)
+  } catch (error){
+    console.error('Error fecthing posts:', error);
   }
-  
-  return {
-    props: {
-      data,
-    },
-  };
   return (
     <div className="min-h-screen max-w-xl mx-auto border-r border-l">
       <div className="py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
